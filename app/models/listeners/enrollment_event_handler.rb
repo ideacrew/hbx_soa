adf45aa02c82c10d79dbc6f6c7aa8e9eb305c3f4
Trey Evans <lewis.r.evans@gmail.com>
2014-11-05 16:59:26 -0500

    Beginning work on enrollment handler.

--- app/models/listeners/enrollment_event_handler.rb
+++ app/models/listeners/enrollment_event_handler.rb
@@ -1,7 +1,21 @@
 module Listeners
   class EnrollmentEventHandler < Amqp::Client
+    def validate(delivery_info, properties, payload)
+      if properties.headers["enrollment_group_uri"].blank?
+        add_error("No enrollment_group_uri")
+      end
+    end
+
     def on_message(delivery_info, properties, payload)
-      
+      enrollment_group_uri = properties.headers['enrollment_group_uri']
+      eg_id = Maybe.new(enrollment_group_uri).split(":").split("#").last.value
+      enrollment_props = {
+        :routing_key => "enrollment.get_by_id",
+        :headers => {
+          :enrollment_id => eg_id
+        }
+      }
+      di, prop, enrollment = request(enrollment_props, "", 30)
       channel.acknowledge(delivery_info.delivery_tag, false)
     end
 
