8f88625343f15f61f9599b3a657994b7997f4a92
Trey Evans <lewis.r.evans@gmail.com>
2014-11-14 15:57:55 -0500

    Listener should be done.

--- app/models/listeners/enrollment_event_handler.rb
+++ app/models/listeners/enrollment_event_handler.rb
@@ -16,7 +16,48 @@ module Listeners
         }
       }
       di, prop, enrollment = request(enrollment_props, "", 30)
+      handle_enrollment_response(prop, enrollment, properties.headers.to_hash.dup)
       channel.acknowledge(delivery_info.delivery_tag, false)
+    end
+
+    def create_enrollment(enrollment_payload, original_headers, qr_uri)
+       request_props = {
+         :routing_key => "enrollment.create",
+         :headers => {
+           :qualifying_reason_uri => qr_uri
+         }
+       }
+
+       di, prop, payload = request(request_props, enrollment_payload, 30)
+       return_code = prop.headers["return_status"]
+       case return_code
+       when "200"
+       else
+        channel.direct(ExchangeInformation.request_exchange, {:durable_change => true}).publish(
+          payload,
+          {
+            :routing_key => "enrollment.error",
+            :headers => original_headers.merge({:return_status => return_status})
+          }
+        )
+      end
+    end
+
+    def handle_enrollment_response(props, payload, original_headers)
+      qr_uri = props.headers["qualifying_reason_uri"]
+      return_code = props.headers["return_status"]
+      case return_code
+      when "200"
+        create_enrollment(payload, original_headers, qr_uri)
+      else
+        channel.direct(ExchangeInformation.request_exchange, {:durable_change => true}).publish(
+          payload,
+          {
+            :routing_key => "enrollment.error",
+            :headers => original_headers.merge({:return_status => return_status})
+          }
+        )
+      end
     end
 
     def self.queue_name
