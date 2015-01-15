2960095d873e8244fc0f4312919a78c512561932
Trey Evans <lewis.r.evans@gmail.com>
2015-01-14 19:16:09 -0500

    Working on processing submitted congresionals.

--- app/models/listeners/enrollment_submitted_handler.rb
+++ app/models/listeners/enrollment_submitted_handler.rb
@@ -5,13 +5,39 @@ module Listeners
       { "cv" => "http://openhbx.org/api/terms/1.0" }
     end
 
+    def get_employment_eligibility_elements(doc)
+      employer_fein = Maybe.new(doc.at_xpath("//cv:employer_link/cv:id/cv:id",xml_ns)).content.strip.split("#").last.value
+      subscriber_ssn = Maybe.new(
+        doc.at_xpath("//cv:enrollee[contains(cv:is_subscriber, 'true')]/cv:member/cv:person_demographics/cv:ssn",xml_ns)
+      ).content.strip.value
+      subscriber_coverage_start = Maybe.new(
+        doc.at_xpath("//cv:enrollee[contains(cv:is_subscriber, 'true')]/cv:benefit/cv:begin_date",xml_ns)
+      ).content.strip.value
+      [employer_fein,subscriber_ssn,subscriber_coverage_start]
+    end
+
+    def fix_start_dates(doc, employment)
+      coverage_start = Date.new(employment.start_date.year, employment.start_date.month, 1) >> 1
+      doc.xpath("//cv:benefit/cv:begin_date", xml_ns).each do |node|
+        node.content = coverage_start.strftime("%Y%m%d")
+      end
+      doc
+    end
+
     def on_message(delivery_info, properties, payload)
       parsed_payload = Nokogiri::XML(payload)
       new_payload = substitute_employer_uri(parsed_payload)
       with_m_ids_payload = generate_member_ids(new_payload)
       with_ids_payload = generate_policy_ids(with_m_ids_payload)
-      create_enrollment(with_ids_payload.canonicalize, properties.headers.to_hash)
-      channel.acknowledge(delivery_info.delivery_tag, false)
+      elig_info = get_employment_eligibility_elements(doc)
+      employment = EmploymentList.match(*elig_info)
+      if employment.blank?
+        fail_with_no_employment(with_ids_payload, elig_info, properties.headers.to_hash)
+      else
+        create_enrollment(fix_start_dates(with_ids_payload, employment), properties.headers.to_hash)
+      end
+      #create_enrollment(with_ids_payload.canonicalize, properties.headers.to_hash)
+      #channel.acknowledge(delivery_info.delivery_tag, false)
     end
 
     def replace_with_map(doc, id_mapping)
@@ -75,6 +101,16 @@ module Listeners
       return doc if all_ids.length < 1
       new_ids = ExchangeSequence.generate_identifiers("policy_id", all_ids.length)
       replace_uris(doc, all_ids, new_ids)
+    end
+
+    def fail_with_no_employment(enrollment_payload, elig_info, original_headers)
+      failure_data = {
+        :reason => "no matching employment",
+        :employer_fein => elig_info.first,
+        :subscriber_ssn => elig_info[1],
+        :coverage_start => elig_info.last 
+      }
+      
     end
 
     def create_enrollment(enrollment_payload, original_headers)
--- spec/app/models/listeners/enrollment_submitted_handler_spec.rb
+++ spec/app/models/listeners/enrollment_submitted_handler_spec.rb
@@ -4,6 +4,28 @@ describe Listeners::EnrollmentSubmittedHandler do
   let(:xml_ns) { { "cv" => "http://openhbx.org/api/terms/1.0" } }
   let(:listener) { Listeners::EnrollmentSubmittedHandler.new(nil, nil) }
 
+  describe "which finds coverage elements" do
+    let(:input_xml) { f = File.open(File.join(Padrino.root, "spec/data/submitted_enrollment.xml")); Nokogiri::XML(f) }
+
+    it "should provide the correct coverage elements" do
+       allow(UriReference).to receive(:resolve_uri).with("urn:dc0:terms:v1:employer_names#senate").and_return("some_id")
+       doc = listener.substitute_employer_uri(input_xml)
+       e_elements = listener.get_employment_eligibility_elements(doc)
+       expect(e_elements).to eq ["some_id","112233445","20150101"]
+    end
+  end
+
+  describe "which has a matching employment" do
+    let(:enrollment_match) { instance_double("Hack::Employment", :start_date => Date.new(2015, 1, 15) ) }
+
+    let(:input_xml) { f = File.open(File.join(Padrino.root, "spec/data/submitted_enrollment.xml")); Nokogiri::XML(f) }
+
+    it "should provide the correct coverage date" do
+      result = listener.fix_start_dates(input_xml, enrollment_match)
+      expect(result.at_xpath("//cv:enrollee[contains(cv:is_subscriber, 'true')]/cv:benefit/cv:begin_date[text()='20150201']",xml_ns).blank?).to be_falsey
+    end 
+  end
+
   describe "which replaces member ids" do
     let(:input_xml) { f = File.open(File.join(Padrino.root, "spec/data/submitted_enrollment.xml")); Nokogiri::XML(f) }
 
