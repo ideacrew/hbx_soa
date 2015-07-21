293126fd986f2c03c962ca2de593ccff0be37f4f
Trey Evans <lewis.r.evans@gmail.com>
2015-07-21 12:26:12 -0400

    Fixing deploy for cloud.

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url, 'git@dchbx.info:repos/hbx_soa.git'
 set :deploy_to, '/var/www/deployments/hbx_soa'
 
 # Default value for :scm is :git
-set :scm, :gitcopy
+# set :scm, :gitcopy
 
 # Default value for :format is :pretty
 # set :format, :pretty
